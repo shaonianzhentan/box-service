8492e559a731e3fb20bc156abaeae89aa361c099
zhulu <shaonianzhentan@sina.cn>
2021-03-17 23:12:18 +0800

    up

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const axios = require('axios')
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use(express.static('public'))
 
